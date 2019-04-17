415ec281fba906fad853d535ba994486966c534d
Samuel Kortas <samuel.kortas@gmail.com>
2019-04-17 04:39:58 +0300

    adding creation of /home/hub/.local/share/jupyter /home/hub/.jupyter/lab for other user to connect correctly

--- kslhub/kslhub_frontend.py
+++ kslhub/kslhub_frontend.py
@@ -77,8 +77,8 @@ def start():
       
     if not(os.path.exists("jobs")):
       cmd = """
-         mkdir -p jobs logs runtime ~/.jupyter/migrated
-         chmod 777 jobs logs runtime ~/.jupyter/migrated
+         mkdir -p jobs logs runtime ~/.jupyter/migrated /home/hub/.local/share/jupyter /home/hub/.jupyter/lab
+         chmod 777 jobs logs runtime ~/.jupyter/migrated /home/hub/.local/share/jupyter /home/hub/.jupyter/lab
       """
       os.system(cmd)
       print("creating working directory jobs, runtime and logs")
